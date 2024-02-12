19ae0c71c53d6f7c0392bb72d1b9a49c9b164404
Andrey Polischuk <me@andrepolischuk.com>
2024-02-12 20:44:50 +0300

    fix(eslint-config): disable stories' default export rule

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -72,7 +72,8 @@ module.exports = {
       files: ['**/__stories__/**', '*.story.*'],
       rules: {
         'import/no-unused-modules': 'off',
-        'import/no-anonymous-default-export': 'off'
+        'import/no-anonymous-default-export': 'off',
+        'storybook/default-exports': 'off'
       }
     }
   ]
